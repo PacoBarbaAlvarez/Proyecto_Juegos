8dfb89f33558b116c4efb0d6adc21f81944a3fb5
pacob <pacobarbitaalvarez@gmail.com>
2024-02-12 18:52:48 +0100

    final3

--- app/src/main/java/com/example/proyecto_juegos/Listado.kt
+++ app/src/main/java/com/example/proyecto_juegos/Listado.kt
@@ -3,6 +3,7 @@ package com.example.proyecto_juegos
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.proyecto_juegos.adapter.ProductoAdapter
 import com.example.proyecto_juegos.databinding.ActivityListadoBinding
@@ -15,10 +16,12 @@ class Listado : ActivityWithMenus() {
     private lateinit var binding: ActivityListadoBinding
     private val db = FirebaseFirestore.getInstance()
     private lateinit var productoAdapter: ProductoAdapter
+    private lateinit var productosList: MutableList<Productos>
 
     // Método onCreate para inicializar la actividad
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         // Inflar el diseño de la actividad y establecerlo como contenido de la vista
         binding = ActivityListadoBinding.inflate(layoutInflater)
@@ -29,6 +32,15 @@ class Listado : ActivityWithMenus() {
         productoAdapter = ProductoAdapter(emptyList()) // Adaptador inicialmente vacío
         binding.recycler.adapter = productoAdapter
 
+        // Agrega un listener al campo de filtro para realizar búsquedas dinámicas
+        binding.filtro.addTextChangedListener { filtro ->
+            val textoFiltro = filtro.toString() // Convertir el Editable a String
+            productoAdapter.actualizarOfertas(productosList.filter { producto ->
+                producto.Nombre.lowercase().contains(textoFiltro.lowercase())
+            })
+        }
+
+
         // Obtener los datos de Firebase
         obtenerDatosFirebase()
     }
@@ -38,7 +50,7 @@ class Listado : ActivityWithMenus() {
         db.collection("Productos")
             .get()
             .addOnSuccessListener { result ->
-                val productosList = mutableListOf<Productos>()
+                productosList = mutableListOf()
                 for (document in result) {
                     // Obtener nombre, descripción y precio del documento y crear un objeto Productos
                     val nombre = document.getString("nombre") ?: ""
